6ff13895bb5628473903eb59629029b7d6a151ff
Joel Kaneshiro <joel8800@gmail.com>
2023-01-12 22:42:39 -0800

    Completed Day01, added README.md-Updater

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine("Day01: Not Quite Lisp");
+
+string input = File.ReadAllText("input.txt");
+
+int floor = 0;
+foreach (char c in input)
+{
+    if (c == '(')
+        floor++;
+    else if (c == ')')
+        floor--;
+}
+Console.WriteLine($"Part1: {floor}");
+
+floor = 0;
+int index = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    char c = input[i];
+    if (c == '(')
+        floor++;
+    else if (c == ')')
+        floor--;
+    index++;
+    if (floor == -1)
+        break;
+}
+Console.WriteLine($"Part2: {index}");
