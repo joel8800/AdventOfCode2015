10df4a11289a15c5e90696dba75357ec948f3c4f
Joel Kaneshiro <joel8800@gmail.com>
2023-01-17 23:23:53 -0800

    Completed Day05

---
+++ Day05/Program.cs
@@ -0,0 +1,81 @@
+﻿using AoCUtils;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Day05: Doesn't He Have Intern-Elves For This?");
+
+string[] input = FileUtil.ReadFileByLine("input.txt");
+
+int nicePt1 = 0;
+foreach (string line in input)
+{
+    if (HasIllegalCombo(line))
+        continue;
+
+    if (Has3Vowels(line) && HasDoubleLetter(line))
+        nicePt1++;
+}
+
+
+int nicePt2 = 0;
+foreach (string line in input)
+{
+    if (HasPairSplitByOneChar(line) && HasTwoPair(line))
+        nicePt2++;
+}
+
+
+Console.WriteLine($"Part1: {nicePt1}");
+Console.WriteLine($"Part2: {nicePt2}");
+
+
+//=============================================================================
+
+bool HasIllegalCombo(string line)
+{
+    MatchCollection mc = Regex.Matches(line, @"ab|cd|pq|xy");
+
+    if (mc.Count > 0)
+        return true;
+    else
+        return false;
+}
+
+bool Has3Vowels(string line)
+{
+    MatchCollection mc = Regex.Matches(line, @"[aeiou]");
+
+    if (mc.Count >= 3)
+        return true;
+    else
+        return false;
+}
+
+bool HasDoubleLetter(string line)
+{
+    MatchCollection mc = Regex.Matches(line, @"(.)\1");
+
+    if (mc.Count > 0)
+        return true;
+    else
+        return false;
+}
+
+bool HasTwoPair(string line)
+{
+    MatchCollection mc = Regex.Matches(line, @"(.)(.).*\1\2");
+
+    if (mc.Count > 0)
+        return true;
+    else
+        return false;
+}
+
+bool HasPairSplitByOneChar(string line)
+{
+    MatchCollection mc = Regex.Matches(line, @"(.).\1");
+
+    if (mc.Count > 0)
+        return true;
+    else
+        return false;
+}
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Advent of Code 2015
 - My attempt to catch up on all the Advents of Code. I'm starting this in 2022 
-- update test
+- 
 
-## Progression:  ![Progress](https://progress-bar.dev/3/?scale=25&title=projects&width=240&suffix=/25)
+## Progression:  ![Progress](https://progress-bar.dev/6/?scale=25&title=projects&width=240&suffix=/25)
 
 
 | Day                                                          | C#                            | Stars |  Solution Description |
@@ -10,3 +10,6 @@
 | [Day 01:  Not Quite Lisp](https://adventofcode.com/2015/day/1) | [Solution](./Day01/Program.c) | :star::star: |
 | [Day 02:  I Was Told There Would Be No Math](https://adventofcode.com/2015/day/2) | [Solution](./Day02/Program.c) | :star::star: |
 | [Day 03:  Perfectly Spherical Houses in a Vacuum](https://adventofcode.com/2015/day/3) | [Solution](./Day03/Program.c) | :star::star: |
+| [Day 04:  The Ideal Stocking Stuffer](https://adventofcode.com/2015/day/4) | [Solution](./Day04/Program.c) | :star::star: |
+| [Day 05:  Doesn&apos;t He Have Intern-Elves For This?](https://adventofcode.com/2015/day/5) | [Solution](./Day05/Program.c) | :star::star: |
+| [Day 06:  Probably a Fire Hazard](https://adventofcode.com/2015/day/6) | [Solution](./Day06/Program.c) |    |
