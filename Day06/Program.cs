04c71a4d786ece459743697ee203a1dbf76d8337
Joel Kaneshiro <joel8800@gmail.com>
2023-01-17 23:25:01 -0800

    Prep for Day06

---
+++ Day06/Program.cs
@@ -0,0 +1,23 @@
+﻿using AoCUtils;
+
+Console.WriteLine("Day06: Probably a Fire Hazard");
+
+string[] input = FileUtil.ReadFileByLine("input.txt");
+
+
+List<(int cm, int x1, int x2, int y1, int y2)> instructions = new();
+foreach (string line in input)
+{
+    int cm = 0;
+
+    if (line.StartsWith("turn off")) cm = 0;
+    if (line.StartsWith("turn on")) cm = 1;
+    if (line.StartsWith("toggle")) cm = 2;
+    
+    
+
+}
+
+const int xSize = 1000;
+const int ySize = 1000;
+bool[,] grid = new bool[xSize, ySize];
