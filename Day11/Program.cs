ce9e63812b0cd9a16f55400474007260edd2a2dd
Joel Kaneshiro <joel8800@gmail.com>
2023-03-28 22:23:12 -0700

    Completed Day11

---
+++ Day11/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Day11: Corporate Policy");
+
+string input = File.ReadAllText("input.txt");
+
+
+
+
+
+string answerPt1 = "";
+
+Console.WriteLine($"Part1: {answerPt1}");
+
+//=============================================================================
+
+string Increment(string input)
+{
+    return "";
+}
