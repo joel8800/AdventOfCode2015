19287f31fe7bbd67f833d5673cbde0c73c316ef3
Joel Kaneshiro <joel8800@gmail.com>
2023-04-28 21:17:07 -0700

    Completed Day12

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -1,98 +1,94 @@
-﻿using System.Text.Json;
-using System.Text.Json.Nodes;
-using AoCUtils;
-using Day12;
+﻿using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
 
 Console.WriteLine("JSAbacusFramework.io");
 string jsonString = File.ReadAllText("input.txt");
-//string jsonString = @"[1,2,3]";
-//string jsonString = @"[{""a"":2,""b"":4}]";
-//string jsonString = @"[[[3]]]";
-//string jsonString = @"{""a"":{ ""b"":4},""c"":-1}";
-//string jsonString = @"{""a"":[-1,1]}";
-//string jsonString = @"[-1,{""a"":1}]";
-//string jsonString = @"[]";
-//string jsonString = @"{}";
 
-JsonNode node = JsonNode.Parse(jsonString);
-var options = new JsonSerializerOptions { WriteIndented = true };
-Console.WriteLine(node!.ToJsonString(options));
+// For Part1, it's easier to use regex to find all the numbers in
+// in the JSON string and add them up
+int total = 0;
+MatchCollection mc = Regex.Matches(jsonString, @"-?\d+");
+foreach (Match m in mc)
+    total += Convert.ToInt32(m.Value);
 
-// Get a JSON object from a JsonNode.
-JsonNode temperatureRanges = node!["TemperatureRanges"]!;
-Console.WriteLine($"Type={temperatureRanges.GetType()}");
-Console.WriteLine($"JSON={temperatureRanges.ToJsonString()}");
-//output:
-//Type = System.Text.Json.Nodes.JsonObject
-//JSON = { "Cold":{ "High":20,"Low":-10},"Hot":{ "High":60,"Low":20} }
+Console.WriteLine($"Part1: {total}");
 
-// Get a JSON array from a JsonNode.
-JsonNode datesAvailable = node!["DatesAvailable"]!;
-Console.WriteLine($"Type={datesAvailable.GetType()}");
-Console.WriteLine($"JSON={datesAvailable.ToJsonString()}");
-//output:
-//datesAvailable Type = System.Text.Json.Nodes.JsonArray
-//datesAvailable JSON =["2019-08-01T00:00:00", "2019-08-02T00:00:00"]
+// ----------------------------------------------------------------------------
+// For Part2, I need to actually parse the JSON string
 
-// Get an array element value from a JsonArray.
-JsonNode firstDateAvailable = datesAvailable[0]!;
-Console.WriteLine($"Type={firstDateAvailable.GetType()}");
-Console.WriteLine($"JSON={firstDateAvailable.ToJsonString()}");
-//output:
-//Type = System.Text.Json.Nodes.JsonValue`1[System.Text.Json.JsonElement]
-//JSON = "2019-08-01T00:00:00"
+JsonNode jsonData = JsonNode.Parse(jsonString);
 
-//// Parse a JSON array
-//var datesNode = JsonNode.Parse(@"[""2019-08-01T00:00:00"",""2019-08-02T00:00:00""]");
-//JsonNode firstDate = datesNode![0]!.GetValue<DateTime>();
-//Console.WriteLine($"firstDate={firstDate}");
-////output:
-////firstDate = "2019-08-01T00:00:00"
+total = 0;
+var jsonArray = jsonData.AsArray();
 
-//// Get a JSON object from a JsonNode.
-//JsonNode temperatureRanges = forecastNode!["TemperatureRanges"]!;
-//Console.WriteLine($"Type={temperatureRanges.GetType()}");
-//Console.WriteLine($"JSON={temperatureRanges.ToJsonString()}");
-////output:
-////Type = System.Text.Json.Nodes.JsonObject
-////JSON = { "Cold":{ "High":20,"Low":-10},"Hot":{ "High":60,"Low":20} }
+foreach (var jNode in jsonArray)
+    total += GetValueOfNode(jNode);
 
-//// Get value from a JsonNode.
-//JsonNode temperatureNode = forecastNode!["Temperature"]!;
-//Console.WriteLine($"Type={temperatureNode.GetType()}");
-//Console.WriteLine($"JSON={temperatureNode.ToJsonString()}");
-////output:
-////Type = System.Text.Json.Nodes.JsonValue`1[System.Text.Json.JsonElement]
-////JSON = 25
-
-//// Get a typed value from a JsonNode.
-//int temperatureInt = (int)forecastNode!["Temperature"]!;
-//Console.WriteLine($"Value={temperatureInt}");
-////output:
-////Value=25
-
-//// Get a typed value from a JsonNode by using GetValue<T>.
-//temperatureInt = forecastNode!["Temperature"]!.GetValue<int>();
-//Console.WriteLine($"TemperatureInt={temperatureInt}");
-////output:
-////Value=25
-
-
-
-
-
-
-
-
-
-
-
-Console.WriteLine();
+Console.WriteLine($"Part2: {total}");
 
 //=============================================================================
 
+// Gets the value of a JsonNode. Checks whether it's a JsonArray or JsonObject
+// Recurses until it resolves every node
+// If a JsonObject contains the string "red", return 0 for everything in that object
+// and its children
+int GetValueOfNode(JsonNode jNode)
+{
+    int nodeValue = 0;
 
-//int sumOfNumbers = JSONReaderNet.ReadFile("input.txt");
-//Console.WriteLine($"Part1: {sumOfNumbers}");
+    if (jNode.GetType().Name == "JsonArray")
+    {
+        foreach (var jn in jNode.AsArray())
+        {
+            if (jn == null)
+                continue;
 
-//=============================================================================
+            if (jn.GetType().Name.Contains("Value"))
+            {
+                if (jn.AsValue().TryGetValue<int>(out int n))
+                {
+                    nodeValue += n;
+                }
+            }
+            else
+            {
+                // not a value type so might be an array or object, recurse
+                nodeValue += GetValueOfNode(jn);
+            }
+        }
+    }
+    else if (jNode.GetType().Name == "JsonObject")
+    {
+        foreach (var kvp in jNode.AsObject())
+        {
+            int retVal = GetValueOfNode(kvp.Value);
+            if (retVal == Int32.MaxValue)
+                return 0;
+            else
+                nodeValue += GetValueOfNode(kvp.Value);
+        }
+    }
+    else
+    {
+        // jNode is neither an array or object so try to resolve it to an int or string
+        if (jNode.AsValue().TryGetValue<int>(out int n))
+        {
+            return n;
+        }
+        else if (jNode.AsValue().TryGetValue<string>(out string? s))
+        {
+            if (s == "red")
+            {
+                return Int32.MaxValue;
+            }
+            return 0;
+        }
+        else
+        {
+            Console.WriteLine("Error: node is neither int or string");
+            return 0;
+        }
+     }
+
+    return nodeValue;
+}
