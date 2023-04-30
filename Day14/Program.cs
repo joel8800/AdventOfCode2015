b3a5f452ccd351aa8c6815be61c0fac71f081b43
Joel Kaneshiro <joel8800@gmail.com>
2023-04-29 20:06:48 -0700

    Completed Day14

---
+++ Day14/Program.cs
@@ -0,0 +1,46 @@
+﻿using AoCUtils;
+using Day14;
+
+Console.WriteLine("Reindeer Olympics");
+
+string[] input = FileUtil.ReadFileByLine("input.txt");
+int raceTime = 2503;
+
+List<Reindeer> reindeer = new();
+foreach (string line in input)
+{
+    string[] parts = line.Split(' ');
+
+    Reindeer rd = new()
+    {
+        Name = parts[0],
+        Speed = Convert.ToInt32(parts[3]),
+        Duration = Convert.ToInt32(parts[6]),
+        Rest = Convert.ToInt32(parts[13])
+    };
+    reindeer.Add(rd);
+}
+
+reindeer.ForEach(r => r.RaceForDistance(raceTime));
+
+int answerPt1 = reindeer.Max(r => r.Distance);
+Console.WriteLine($"Part1: {answerPt1}");
+
+// ----------------------------------------------------------------------------
+
+reindeer.ForEach(r => r.Start());
+
+for (int i = 0; i < raceTime; i++)
+{
+    reindeer.ForEach(r => r.DistanceAfterOneSecond());
+
+    // give a point to the leader(s)
+    int leadDistance = reindeer.Max(r => r.Distance);
+    List<Reindeer> leaders = reindeer.Where(r => r.Distance == leadDistance).ToList();
+    leaders.ForEach(r => r.Points++);
+}
+
+int answerPt2 = reindeer.Max(r => r.Points);
+Console.WriteLine($"Part2: {answerPt2}");
+
+// ============================================================================
---
+++ Day14/Reindeer.cs
@@ -0,0 +1,81 @@
+﻿namespace Day14
+{
+    public class Reindeer
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Duration { get; set; }
+        public int Rest { get; set; }
+        public int Distance { get; set; }
+        public int Points { get; set; }
+        public int CurrentDuration { get; set; }
+        public bool IsResting { get; set; }
+
+        public Reindeer() 
+        {
+            Name = string.Empty;
+            Speed = 0;
+            Duration = 0;
+            Rest = 0;
+            Distance = 0;
+            Points = 0;
+            IsResting = false;
+            CurrentDuration = 0;
+        }
+
+        public void Start()
+        {
+            Distance = 0;
+            Points = 0;
+            IsResting = false;
+            CurrentDuration = 0;
+        }
+
+        public void RaceForDistance(int timeInSeconds)
+        {
+            Start();
+            int timeInterval = Duration + Rest;
+            int distInterval = Speed * Duration;
+            int wholeIntervals = 0;
+            int remTime = 0;
+            
+            if (timeInterval < timeInSeconds)
+            {
+                wholeIntervals = timeInSeconds / timeInterval;
+                remTime = timeInSeconds % timeInterval;
+            }
+
+            int remDist;
+            if (remTime <= Duration)
+                remDist = remTime * Speed;
+            else
+                remDist = distInterval;
+
+            Distance = wholeIntervals * distInterval + remDist;
+        }
+
+        public int DistanceAfterOneSecond()
+        {
+            CurrentDuration++;
+            if (IsResting)
+            {
+                if (CurrentDuration >= Rest)
+                {
+                    CurrentDuration = 0;
+                    IsResting = false;
+                }
+            }
+            else
+            {
+                Distance += Speed;
+                if (CurrentDuration >= Duration) 
+                {
+                    CurrentDuration = 0;
+                    IsResting = true;
+                }                
+            }
+
+            return Distance;
+        }
+    }
+}
