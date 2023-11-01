f70b15dded9a4168b15df600f742825d82113fb0
Joel Kaneshiro <joel8800@gmail.com>
2023-10-31 20:30:45 -0700

    Added project for Day24

---
+++ Day24/Program.cs
@@ -0,0 +1,14 @@
+﻿using AoCUtils;
+
+Console.WriteLine("Day24: It Hangs in the Balance");
+
+string[] input = FileUtil.ReadFileByLine("inputSample.txt");
+
+List<int> packages = new();
+foreach (string line in input)
+    packages.Add(int.Parse(line));
+
+
+
+Console.WriteLine($"Part1: {0}");
+Console.WriteLine($"Part2: {0}");
