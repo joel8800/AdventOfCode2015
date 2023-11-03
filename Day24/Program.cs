4928c3e26c7eb2dc3671138228b50229378d1288
Joel Kaneshiro <joel8800@gmail.com>
2023-11-02 21:54:20 -0700

    Completed Day 24

--- Day24/Program.cs
+++ Day24/Program.cs
@@ -5,46 +5,14 @@ Console.WriteLine("Day24: It Hangs in the Balance");
 
 string[] input = FileUtil.ReadFileByLine("input.txt");
 
-Sleigh sleigh = new(input, 3);
+Sleigh sleighPt1 = new(input, 3);
+sleighPt1.FindCombinations();
 
-sleigh.FindCombos(true);
-
-//int qe = 0;
-
-
-//Console.WriteLine($"Part1: {qe}");
-//Console.WriteLine($"Part2: {0}");
-
-Console.WriteLine();
-Console.WriteLine();
-Console.WriteLine();
+Sleigh sleighPt2 = new(input, 4);
+sleighPt2.FindCombinations();
 
 
-//=============================================================================
+Console.WriteLine($"Part1: {sleighPt1.QE}");
+Console.WriteLine($"Part2: {sleighPt2.QE}");
 
-static int SumCounter(List<int> numbers, int target)
-{
-    int result = 0;
-    RecursiveCounter(numbers, target, new List<int>(), result); //ref result);
-    return result;
-}
-
-static void RecursiveCounter(List<int> numbers, int target, List<int> partial, int result)//ref int result)
-{
-    int sum = partial.Sum();
-    if (sum == target)
-    {
-        result++;
-        Console.WriteLine(string.Join(" ", partial.ToArray()));
-    }
-
-    if (sum >= target) return;
-
-    for (var i = 0; i < numbers.Count; i++)
-    {
-        List<int> remaining = new();
-        for (var j = i + 1; j < numbers.Count; j++) remaining.Add(numbers[j]);
-        List<int> partRec = new(partial) { numbers[i] };
-        RecursiveCounter(remaining, target, partRec, result);//ref result);
-    }
-}
+//=============================================================================}
--- Day24/Sleigh.cs
+++ Day24/Sleigh.cs
@@ -1,219 +1,87 @@
-﻿using System.Reflection.Metadata.Ecma335;
-
-namespace Day24
+﻿namespace Day24
 {
     internal class Sleigh
     {
-        private HashSet<List<int>> group1;
-        private HashSet<List<int>> group2;
-        private HashSet<List<int>> group3;
+        private int TargetWeight;
+        private List<int> PackageWeights;
+        private HashSet<List<int>> mainCompartment;
 
-        private int minLength;
-
-        public int TargetWeight { get; set; }
         public int Compartments { get; private set; }
-        public long QE { get; set; }
-
-        public List<int> Packages;
+        public long QE { get; private set; }
 
 
         public Sleigh(string[] input, int compartments)
         {
-            minLength = input.Length;
             Compartments = compartments;
 
             // convert strings in input to integers
-            Packages = input.Select(x => int.Parse(x)).ToList();
+            PackageWeights = input.Select(x => int.Parse(x)).ToList();
 
             // sort in descending order
-            Packages.Sort();
-            Packages.Reverse();
+            PackageWeights.Sort();
+            PackageWeights.Reverse();
 
-            PrintPackages();
-
-            int totalWeight = Packages.Sum();
+            // set target weight
+            int totalWeight = PackageWeights.Sum();
             TargetWeight = totalWeight / Compartments;
 
-            if (totalWeight % compartments == 0)
-                Console.WriteLine($"total weight = {totalWeight}, group weight = {TargetWeight}");
-            else
-                Console.WriteLine($"*** Error: Total weight does not divide by {Compartments} evenly");
-
-            group1 = new();
-            group2 = new();
-            group3 = new();
-        }
-
-        public void PrintPackages()
-        {
-            foreach (int i in Packages)
-                Console.Write($"{i} ");
-            Console.WriteLine();
+            mainCompartment = new();
         }
 
         public void PrintList(List<int> list)
         {
-            long qe = CalcQuantumEntanglement(list);
-
-            foreach (int i in list)
-                Console.Write($"{i,3} ");
-            Console.WriteLine($"  \t(QE={qe})");
+            list.ForEach(i => Console.Write($"{i,3} "));
+            Console.WriteLine();
         }
 
-        public void FindCombos(bool isMain)
+        public void FindCombinations()
         {
-            group1 = new();
-            group2 = new();
-            group3 = new();
+            mainCompartment = new();
 
-            RecursiveFind(Packages, new List<int>(), isMain);
+            // find all combinations for group1
+            RecursiveFind(PackageWeights, new List<int>());
 
-            //Console.WriteLine("Combos found");
-            //foreach (List<int> list in cache)
-            //    PrintList(list);
-
-            //Console.WriteLine($"Combinations: {cache.Count}");
-            //Console.WriteLine("------------");
-
-            int minCount = group1.Min(x => x.Count);
-            List<List<int>> shortest = group1.Where(x => x.Count == minCount).ToList();
+            // keep only the shortest list(s)
+            int minCount = mainCompartment.Min(x => x.Count);
+            List<List<int>> shortest = mainCompartment.Where(x => x.Count == minCount).ToList();
             
-            foreach (List<int> list in shortest)
-                PrintList(list);
-
-            Console.WriteLine($"Shortest: {shortest.Count}  length:{shortest.First().Count}");
-
-            //// validate each combo to make sure that they balance
-            //group1.Clear();
-            //foreach (List<int> list in shortest)
-            //{
-            //    if (IsBalanceable(list))
-            //    {
-            //        group1.Add(list);
-            //    }
-            //}
-
+            // calculate the lowest quantum entanglement
             QE = long.MaxValue;
             foreach (List<int> list in shortest)
             {
                 long tempQE = CalcQuantumEntanglement(list);
                 QE = tempQE < QE ? tempQE : QE; 
             }
-
-            Console.WriteLine($"Min quantum entanglement = {QE}");
         }
 
-        private bool IsBalanceable(List<int> list)
-        {
-            Console.WriteLine("-- checking if balanceable");
-
-            List<int> numbers = Packages.Except(list).ToList();
-
-            group2.Clear();
-            RecursiveFind(numbers, new List<int>(), false);
-
-            if (group2.Count == 0)
-                return false;
-            else
-            {
-                if (Compartments <= 3)
-                    return true;
-                else
-                {
-                    group3 = group2.ToHashSet();
-                    foreach (List<int> list2 in group2)
-                    {
-                        group2 = new();
-                        numbers = Packages.Except(list).Except(list2).ToList();
-                        RecursiveFind(numbers, new List<int>(), false);
-
-                        if (group2.Count > 0)
-                            return true;
-                    }
-                }
-            }
-            return false;
-        }
-
-        private bool RecursiveFind(List<int> numbers, List<int> partial, bool isMain)
+        private bool RecursiveFind(List<int> numbers, List<int> partial)
         {
             int sum = partial.Sum();
+            
+            // hit target
             if (sum == TargetWeight)
-            {
-                // target hit
-                if (partial.Count <= minLength)
-                {
-                    if (FindNextGroup(partial))
-                    {
-                        if (isMain)
-                            group1.Add(partial);
-                        else 
-                            group2.Add(partial); 
-                        
-                        if (minLength < partial.Count)
-                            minLength = partial.Count;
-                    }
-                }
-                //return true;
-            }
+                mainCompartment.Add(partial);
 
             // overshot target
             if (sum >= TargetWeight)
                 return false;
 
-            // work on remaining numbers
+            // continue with remaining numbers
             for (int i = 0; i < numbers.Count; i++)
             {
-                // start with 2nd element
+                // subset list starting with 2nd element
                 List<int> remaining = new();
+
                 for (int j = i + 1; j < numbers.Count; j++)
                     remaining.Add(numbers[j]);
 
                 List<int> partRec = new(partial) { numbers[i] };
                 
-                if (RecursiveFind(remaining, partRec, isMain))
+                if (RecursiveFind(remaining, partRec))
                     return true;
             }
             return false;
         }
-
-        private bool FindNextGroup(List<int> used)
-        {
-            List<int> numbers = Packages.Except(used).ToList();
-
-            //Console.WriteLine("------");
-            //PrintList(remaining);
-            //PrintList(numbers);
-            //Console.WriteLine("------");
-
-            return RecursiveFindGroup2(numbers, new List<int>());
-        }
-
-        private bool RecursiveFindGroup2(List<int> numbers, List<int> partial)
-        {
-            int sum = partial.Sum();
-            //Console.WriteLine(sum);
-
-            if (sum == TargetWeight)
-                return true;
-
-            if (sum > TargetWeight)
-                return false;
-
-            for (int i = 0; i < numbers.Count; i++)
-            {
-                List<int> remaining = new();
-                for (int j = i + 1; j < numbers.Count; j++)
-                    remaining.Add(numbers[j]);
-
-                List<int> partRec = new(partial) { numbers[i] };
-
-                if (RecursiveFindGroup2(remaining, partRec))
-                    return true;
-            }
-            return false;
-        }
-
 
         private long CalcQuantumEntanglement(List<int> list)
         {
